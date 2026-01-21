9a89448e68e54f09aab2020f044b80e7b800ecd3
신서영 <pyqw_@naver.com>
2026-01-21 22:45:25 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.65 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String strAge = String.valueOf(age);
+        for (int i = 0; i < strAge.length(); i++) {
+            answer += (char)(strAge.charAt(i)+49);
+        }
+        
+        return answer;
+    }
+}
