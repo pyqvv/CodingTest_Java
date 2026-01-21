b0a469e2a9a6d6366456cb04334cf962c82b48b7
신서영 <pyqw_@naver.com>
2026-01-21 15:51:29 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -3,13 +3,7 @@ class Solution {
         for (int i = 0; i < str1.length(); i++) {
             if (str1.charAt(i) == str2.charAt(0)) {
                 if (i+str2.length()<=str1.length()) {
-                    int j = 0;
-                    for (j = 0; j < str2.length(); j++) {
-                        if (str1.charAt(i+j) != str2.charAt(j)) {
-                            break;
-                        }
-                    }
-                    if (j == str2.length())
+                    if (str1.substring(i, i+str2.length()).equals(str2))
                         return 1;
                 }
             }
